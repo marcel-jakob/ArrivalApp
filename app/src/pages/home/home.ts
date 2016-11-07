10e71a84be39516b222d339e1efe18cc941a9d92
ChristophWalter <c.walter-95@gmx.de>
2016-11-07 18:07:44 +0100

    added google maps and link to contacts page

--- app/src/pages/home/home.ts
+++ app/src/pages/home/home.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild, ElementRef} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import {ContactsPage} from '../contacts/contacts';
 
-import { NavController } from 'ionic-angular';
+declare var google;
 
 @Component({
   selector: 'page-home',
@@ -8,8 +10,32 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+
   constructor(public navCtrl: NavController) {
-    
+
+  }
+
+  ionViewDidLoad() {
+    this.loadMap();
+  }
+
+  loadMap() {
+    let latLng = new google.maps.LatLng(48.774369, 9.171814);
+
+    let mapOptions = {
+      center: latLng,
+      zoom: 13,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+  }
+
+  clickSendLocation(event){
+    this.navCtrl.push(ContactsPage);
   }
 
 }
