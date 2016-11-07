dd8d56821990221906d982d5689204b1ed2ffc4b
Marcel Jakob <jakob.marcel@web.de>
2016-11-07 17:58:02 +0100

    Added contacts page with sample persons

---
+++ app/src/pages/contacts/contacts.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-contacts',
+  templateUrl: 'contacts.html'
+})
+export class ContactsPage {
+
+  constructor(public navCtrl: NavController) {}
+  
+
+}
