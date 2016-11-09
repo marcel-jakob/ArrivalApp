9dcc2711dd798291cac9af813710338cf3015198
ChristophWalter <c.walter-95@gmx.de>
2016-11-09 18:16:59 +0100

    added geolocation, but not working

--- app/src/pages/home/home.ts
+++ app/src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import {Component, ViewChild, ElementRef} from '@angular/core';
-import {NavController} from 'ionic-angular';
+import {NavController, Platform} from 'ionic-angular';
 import {ContactsPage} from '../contacts/contacts';
+import {Geolocation} from 'ionic-native';
 
 declare var google;
 
@@ -8,34 +9,54 @@ declare var google;
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
 
   @ViewChild('map') mapElement: ElementRef;
   map: any;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, platform: Platform) {
+    platform.ready().then(() => {
+      this.loadMap();
+    });
   }
 
   ionViewDidLoad() {
-    this.loadMap();
   }
 
   loadMap() {
-    let latLng = new google.maps.LatLng(48.774369, 9.171814);
+    console.log("load map");
+    Geolocation.getCurrentPosition().then((position) => {
+      console.log("location received");
+      console.log(position);
+      let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 
-    let mapOptions = {
-      center: latLng,
-      zoom: 13,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
+      let mapOptions = {
+        center: latLng,
+        zoom: 13,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
 
-    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+    }, (err) => {
+      console.log(err);
+    });
 
   }
 
-  clickSendLocation(){
+  clickSendLocation() {
     this.navCtrl.push(ContactsPage);
+  }
+
+  clickAddMarker() {
+
+    new google.maps.Marker({
+      map: this.map,
+      animation: google.maps.Animation.DROP,
+      position: this.map.getCenter()
+    });
+
   }
 
 }
