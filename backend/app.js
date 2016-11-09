16eaf241662b9f9776dc6a43016114a49f726788
Marcel Jakob <jakob.marcel@web.de>
2016-11-09 17:38:20 +0100

    NeDB connection + started POST /newUser

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var Datastore = require('nedb');
 
 // ### Server Config
 var port = process.env.PORT || 3000; //use 3000 or environment var
@@ -8,11 +9,13 @@ var port = process.env.PORT || 3000; //use 3000 or environment var
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
-// ### Routes
-// route /users
-var usersRoute = require('./routes/users.js');
-app.use('/users', usersRoute);
-
+// ### NeDB connection
+db = new Datastore({ filename: './DB/users.db' });
+db.loadDatabase(function (err) {
+	if(err){
+		console.log("couldn't access DB")
+	}
+});
 
 // ### Startup
 app.listen(port, function (err) {
@@ -23,6 +26,35 @@ app.listen(port, function (err) {
 	console.log("express server started at :" + port);
 
 });
+
+// ### ROUTES --------------------------------------------------------------------------
+
+// POST new User
+app.post('/newUser', function (req, res) {
+	var doc = req.body;
+
+	db.insert(doc, function (err, newDoc) {   // Callback is optional
+		if(err){
+			cosole.log("error at DB insertion");
+		}
+		else{
+			console.log("Inserted new user in DB:");
+			console.log(newDoc);
+			console.log("\n");
+			res.send("ok");
+		}
+	});
+});
+
+// GET all Users
+app.get('/', function (req, res) {
+});
+
+//GET one user by id
+app.get('/:id', function (req, res) {
+});
+
+
 
 //export express app for tests etc.
 module.exports = app;
