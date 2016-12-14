6f873ba687c54ac9d83f33995516d0d79b11df2b
ChristophWalter <c.walter-95@gmx.de>
2016-12-14 21:41:25 +0100

    create events when: own location is updated new user location is found user location updated

--- app/src/app/Services/locationService.ts
+++ app/src/app/Services/locationService.ts
@@ -1,26 +1,93 @@
-import {Injectable} from '@angular/core';
-import {Geolocation} from 'ionic-native';
-import {BackendService} from "./backendService";
+import { Injectable } from '@angular/core';
+import { Geolocation, Geoposition } from 'ionic-native';
+import { BackendService } from "./backendService";
+import { Events } from 'ionic-angular';
 
 @Injectable()
 export class LocationService {
+  private ownLocation;
+  private sharedContacts;
 
-  constructor(private backendService: BackendService) {
+  constructor ( private backendService: BackendService, private events: Events ) {
+    this.sharedContacts = [];
+    this.startWatching();
+    this.watchSharedContacts();
   }
 
-  public upload() {
+  private startWatching () {
     let locationOptions = {
-      timeout: 10000,
+      timeout           : 10000,
       enableHighAccuracy: true
     };
-    Geolocation.getCurrentPosition(locationOptions).then((resp) => {
-      this.backendService.uploadLocation({latitude: resp.coords.latitude, longitude: resp.coords.longitude}).subscribe(
-        error => console.log(error),
-        () => console.log("Request Finished")
-      );
-    }).catch((error) => {
-      console.log('Error getting location', error);
-    })
-
+    Geolocation.watchPosition( locationOptions )
+      .subscribe( ( position: Geoposition ) => {
+        this.ownLocation = {
+          latitude : position.coords.latitude,
+          longitude: position.coords.longitude
+        };
+        this.events.publish( 'ownPosition:updated', this.ownLocation );
+      } );
   }
+
+  public upload () {
+    this.backendService.uploadLocation( this.ownLocation )
+      .subscribe( error => console.log( error ), () => console.log( "Request Finished" ) );
+  }
+
+  private watchSharedContacts () {
+    setInterval( () => {
+      this.backendService.getLocations()
+        .subscribe( response => this.watchSharedContactsResponse( response ),
+          error => this.watchSharedContactsError( error ), () => console.log( "Get Locations Request Finished" ) );
+    }, 5000 );
+  }
+
+  private watchSharedContactsResponse ( response ) {
+    for ( let i = 0; i < response.length; i++ ) {
+      let username = response[ i ].username;
+      let position = {
+        latitude : response[ i ].coordinates.latitude,
+        longitude: response[ i ].coordinates.longitude
+      };
+      this.compareUsernameToSharedContacts( username, position )
+    }
+  }
+
+  private watchSharedContactsError ( error ) {
+    //this.warning = "Es ist ein Fehler bei der für Sie freigegebenen Standorte aufgetreten. Bitte versuchen Sie es erneut";
+    console.log( error );
+  }
+
+  private compareUsernameToSharedContacts ( username, position ) {
+    let notFound = true;
+    for ( let i = 0; i < this.sharedContacts.length; i++ ) {
+      //check if username is already in sharedContacts
+      if ( username === this.sharedContacts[ i ].name ) {
+        notFound = false;
+        //check if position has changed
+        if ( position.latitude != this.sharedContacts[ i ].position.latitude || position.longitude
+          != this.sharedContacts[ i ].position.longitude ) {
+          //save new position
+          this.sharedContacts[ i ].position.latitude = position.latitude;
+          this.sharedContacts[ i ].position.longitude = position.longitude;
+          this.events.publish( 'userPosition:updated', {
+            user    : username,
+            position: position
+          } );
+        }
+      }
+    }
+    if ( notFound ) {
+      //save new shared contact
+      this.sharedContacts.push( {
+        name    : username,
+        position: position
+      } );
+      this.events.publish( 'userPosition:new', {
+        user    : username,
+        position: position
+      } );
+    }
+  }
+
 }
--- app/src/pages/home/home.ts
+++ app/src/pages/home/home.ts
@@ -1,23 +1,23 @@
-import {Component, ViewChild, ElementRef} from '@angular/core';
-import {NavController} from 'ionic-angular';
-import {Storage} from '@ionic/storage';
-import {Geolocation} from 'ionic-native';
-import {BackendService} from "../../app/Services/backendService";
-import {NgZone} from '@angular/core';
-import {LocationService} from "../../app/Services/locationService";
-import {ContactsPage} from '../contacts/contacts';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { NavController, Events } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
+import { Geolocation } from 'ionic-native';
+import { BackendService } from "../../app/Services/backendService";
+import { NgZone } from '@angular/core';
+import { LocationService } from "../../app/Services/locationService";
+import { ContactsPage } from '../contacts/contacts';
 
 
 declare let google;
 
-@Component({
-  selector: 'page-home',
+@Component( {
+  selector   : 'page-home',
   templateUrl: 'home.html'
-})
+} )
 
 export class HomePage {
   //Google Map
-  @ViewChild('map') mapElement: ElementRef;
+  @ViewChild( 'map' ) mapElement: ElementRef;
   private map: any;
   //Warning Message
   public warning: string;
@@ -35,68 +35,68 @@ export class HomePage {
   //intervall to update locations
   private updateInterval;
 
-  constructor(public navCtrl: NavController, private backendService: BackendService, private zone: NgZone, private locationService: LocationService, private storage: Storage) {
+  constructor ( public navCtrl: NavController, private backendService: BackendService, private zone: NgZone,
+                private locationService: LocationService, private storage: Storage, private events: Events ) {
     //initialise the array for shared contacts
     this.sharedContacts = [];
+    this.handleEvents();
   }
 
   //this function is triggered before the home screen is viewed
-  ionViewWillEnter() {
+  ionViewWillEnter () {
 
     //initializing the map and updating it with the position of shared contacts
     this.initMap();
 
     //check if other page changed access to someone
-    this.storage.get('giveAccessTo').then((accessTo) => {
-      if (accessTo) {
-        this.giveAccessTo = accessTo;
-      }
-      else {
-        this.giveAccessTo = "";
-      }
-    });
+    this.storage.get( 'giveAccessTo' )
+      .then( ( accessTo ) => {
+        if ( accessTo ) {
+          this.giveAccessTo = accessTo;
+        } else {
+          this.giveAccessTo = "";
+        }
+      } );
 
     //update positions and routes every 30 seconds
-    this.updateInterval = setInterval(() => {
+    this.updateInterval = setInterval( () => {
       this.updateLocation()
-    }, 20000);
+    }, 20000 );
   }
 
   //this function is triggered before the home screen is leaved
-  ionViewWillLeave() {
-    clearInterval(this.updateInterval);
+  ionViewWillLeave () {
+    clearInterval( this.updateInterval );
   }
 
-  public clickSendLocation() {
-    this.navCtrl.push(ContactsPage);
+  public clickSendLocation () {
+    this.navCtrl.push( ContactsPage );
   }
 
-  public clickContact(contact) {
-    this.calculateRoute(this.ownMarker, contact);
+  public clickContact ( contact ) {
+    this.calculateRoute( this.ownMarker, contact );
   }
 
   /* =====================================
    GIVE ACCESS to contacts
    ===================================== */
-  public clickGiveAccess(contactName) {
-    this.backendService.giveAccess(contactName).subscribe(
-      response => this.handleGiveAccessResponse(contactName),
-      error => this.handleGiveAccessError(error),
-      () => console.log("Request Finished")
-    );
+  public clickGiveAccess ( contactName ) {
+    this.backendService.giveAccess( contactName )
+      .subscribe( response => this.handleGiveAccessResponse( contactName ),
+        error => this.handleGiveAccessError( error ), () => console.log( "Request Finished" ) );
   }
 
-  private handleGiveAccessResponse(contactName) {
+  private handleGiveAccessResponse ( contactName ) {
     //save who got access
-    this.storage.set('giveAccessTo', contactName);
+    this.storage.set( 'giveAccessTo', contactName );
     this.giveAccessTo = contactName;
 
     //updates markers (and routes)
     this.updateLocation();
   }
 
-  private handleGiveAccessError(error) {
-    console.log(error);
+  private handleGiveAccessError ( error ) {
+    console.log( error );
     this.warning = "Es ist ein Fehler bei der Standortfreigabe aufgetreten. Bitte versuchen Sie es erneut";
   }
 
@@ -104,26 +104,17 @@ export class HomePage {
    INITIALISING
    ===================================== */
   //this function initializes the map
-  private initMap() {
+  private initMap () {
     //creating map
     let options = {
-      zoom: 14,
+      zoom     : 14,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     };
-    this.map = new google.maps.Map(
-      this.mapElement.nativeElement,
-      options
-    );
+    this.map = new google.maps.Map( this.mapElement.nativeElement, options );
 
 
     //initialize routing
     this.initRoute();
-
-    //getting own location
-    let locationOptions = {
-      timeout: 10000,
-      enableHighAccuracy: true
-    };
 
     //update location of markers
     this.updateLocation();
@@ -134,49 +125,52 @@ export class HomePage {
    ===================================== */
 
   //this function updates the own marker and triggers getting of other markers
-  private updateLocation() {
-    this.resetMarker();
+  private updateLocation () {
+    //this.resetMarker();
 
     //get own location, set it to map center and create marker
     let locationOptions = {
-      timeout: 10000,
+      timeout           : 10000,
       enableHighAccuracy: true
     };
-    Geolocation.getCurrentPosition(locationOptions).then((resp) => {
-      let position = new google.maps.LatLng(resp.coords.latitude, resp.coords.longitude);
-      this.map.setCenter(position);
-      this.ownMarker = new google.maps.Marker({
-        map: this.map,
-        position: position
-      });
+    Geolocation.getCurrentPosition( locationOptions )
+      .then( ( resp ) => {
+        /*let position = new google.maps.LatLng( resp.coords.latitude, resp.coords.longitude );
+         this.map.setCenter( position );
+         this.ownMarker = new google.maps.Marker( {
+         map     : this.map,
+         position: position
+         } );*/
 
-      //get location of contacts
-      this.getLocations();
-    }).catch((error) => {
-      console.log('Error getting location', error);
-    });
+        //get location of contacts
+        //this.getLocations();
+      } )
+      .catch( ( error ) => {
+        console.log( 'Error getting location', error );
+      } );
   }
 
   //this function gets the location of contacts from the backend
-  private getLocations() {
-    this.backendService.getLocations().subscribe(
-      response => this.handleGetLocationsResponse(response),
-      error => this.handleGetLocationsError(error),
-      () => console.log("Get Locations Request Finished")
-    );
+  private getLocations () {
+    this.backendService.getLocations()
+      .subscribe( response => this.handleGetLocationsResponse( response ),
+        error => this.handleGetLocationsError( error ), () => console.log( "Get Locations Request Finished" ) );
   }
 
-  private handleGetLocationsResponse(response) {
+  private handleGetLocationsResponse ( response ) {
     //clear array of old shared contacts
     this.sharedContacts = [];
     //fill array with shared contacts
-    for (let i = 0; i < response.length; i++) {
-      let position = new google.maps.LatLng(response[i].coordinates.latitude, response[i].coordinates.longitude);
-      let marker = new google.maps.Marker({
-        map: this.map,
+    for ( let i = 0; i < response.length; i++ ) {
+      let position = new google.maps.LatLng( response[ i ].coordinates.latitude, response[ i ].coordinates.longitude );
+      let marker = new google.maps.Marker( {
+        map     : this.map,
         position: position
-      });
-      this.sharedContacts.push({name: response[i].username, marker: marker});
+      } );
+      this.sharedContacts.push( {
+        name  : response[ i ].username,
+        marker: marker
+      } );
     }
     //reset old warning
     this.warning = "";
@@ -186,9 +180,9 @@ export class HomePage {
     this.locationService.upload();
   }
 
-  private handleGetLocationsError(error) {
+  private handleGetLocationsError ( error ) {
     this.warning = "Es ist ein Fehler bei der für Sie freigegebenen Standorte aufgetreten. Bitte versuchen Sie es erneut";
-    console.log(error);
+    console.log( error );
   }
 
 
@@ -196,65 +190,91 @@ export class HomePage {
    CALCULATING ROUTE
    ===================================== */
 
-  private initRoute() {
+  private initRoute () {
     // Create a renderer for directions and bind it to the map.
-    this.directionsDisplay = new google.maps.DirectionsRenderer({map: this.map});
+    this.directionsDisplay = new google.maps.DirectionsRenderer( { map: this.map } );
 
     // Instantiate a directions service.
     this.directionsService = new google.maps.DirectionsService;
   }
 
   //this function calculates the route between two markers
-  private calculateRoute(fromMarker, toContact) {
+  private calculateRoute ( fromMarker, toContact ) {
     this.resetMarker();
 
-    this.directionsService.route({
-      origin: fromMarker.getPosition(),
+    this.directionsService.route( {
+      origin     : fromMarker.getPosition(),
       destination: toContact.marker.getPosition(),
-      travelMode: google.maps.TravelMode.WALKING
-    }, (response, status) => {
-      if (status === google.maps.DirectionsStatus.OK) {
+      travelMode : google.maps.TravelMode.WALKING
+    }, ( response, status ) => {
+      if ( status === google.maps.DirectionsStatus.OK ) {
 
         //using zone to fix variables not updating (google service)
-        this.zone.run(() => {
+        this.zone.run( () => {
 
           //store the active route
           this.calculateRouteTo = toContact;
-          this.calculateRouteTo.info =
-            response.routes[0].legs[0].distance.text +
-            ' - ' +
-            response.routes[0].legs[0].duration.text;
-        });
+          this.calculateRouteTo.info = response.routes[ 0 ].legs[ 0 ].distance.text + ' - '
+            + response.routes[ 0 ].legs[ 0 ].duration.text;
+        } );
 
         //show the route
-        this.directionsDisplay.setDirections(response);
+        this.directionsDisplay.setDirections( response );
       } else {
         this.warning = 'Directions request failed due to ' + status;
       }
-    });
+    } );
   }
 
   //this function recalculates the route if necessary
-  private updateRoute() {
-    if (this.calculateRouteTo) {
+  private updateRoute () {
+    if ( this.calculateRouteTo ) {
       //show if calculateRoue to is in array of shared contacts
-      for (let i = 0; i < this.sharedContacts.length; i++) {
-        if (this.sharedContacts[i].name === this.calculateRouteTo.name) {
-          this.calculateRoute(this.ownMarker, this.sharedContacts[i]);
+      for ( let i = 0; i < this.sharedContacts.length; i++ ) {
+        if ( this.sharedContacts[ i ].name === this.calculateRouteTo.name ) {
+          this.calculateRoute( this.ownMarker, this.sharedContacts[ i ] );
         }
       }
     }
   }
 
   //this function resets all marker from the map
-  private resetMarker() {
-    if (this.ownMarker) {
-      this.ownMarker.setMap(null);
+  private resetMarker () {
+    if ( this.ownMarker ) {
+      this.ownMarker.setMap( null );
     }
-    for (let i = 0; i < this.sharedContacts.length; i++) {
-      this.sharedContacts[i].marker.setMap(null);
-      this.sharedContacts[i].info = null;
+    for ( let i = 0; i < this.sharedContacts.length; i++ ) {
+      this.sharedContacts[ i ].marker.setMap( null );
+      this.sharedContacts[ i ].info = null;
     }
+  }
+
+
+  /* =====================================
+   EVENTS
+   ===================================== */
+
+  private handleEvents () {
+    this.events.subscribe( 'ownPosition:updated', ( ownLocation ) => {
+      let gmapsPosition = {
+        lat: ownLocation[ 0 ].latitude,
+        lng: ownLocation[ 0 ].longitude
+      };
+      if ( this.map ) {
+        if ( !this.ownMarker ) {
+          //create new marker
+          this.map.setCenter( gmapsPosition );
+          this.ownMarker = new google.maps.Marker( {
+            map     : this.map,
+            position: gmapsPosition
+          } );
+        } else {
+          //update marker position
+          this.map.setCenter( gmapsPosition );
+          this.ownMarker.setPosition( gmapsPosition );
+        }
+      }
+    } );
   }
 
 }
