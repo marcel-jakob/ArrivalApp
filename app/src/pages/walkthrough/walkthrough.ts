88208aadee722be13ca7d0680bac2ef42a021fe2
ChristophWalter <c.walter-95@gmx.de>
2016-12-27 15:58:23 +0100

    minor bugfixes

--- app/src/pages/walkthrough/walkthrough.ts
+++ app/src/pages/walkthrough/walkthrough.ts
@@ -1,17 +1,22 @@
-import {Component} from '@angular/core';
-import {NavController} from 'ionic-angular';
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
 import { FirststartPage } from '../firststart/firststart';
 
-@Component({
-  selector: 'page-walkthrough',
+@Component( {
+  selector   : 'page-walkthrough',
   templateUrl: 'walkthrough.html'
-})
+} )
 export class WalkthroughPage {
+  public mySlideOptions;
 
-  constructor(public navCtrl: NavController) {
+  constructor ( public navCtrl: NavController ) {
+    this.mySlideOptions = {
+      autoplay: 3000,
+      pager   : true
+    };
   }
 
-  clickNext() {
-    this.navCtrl.push(FirststartPage);
+  clickNext () {
+    this.navCtrl.push( FirststartPage );
   }
 }
